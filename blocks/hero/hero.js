1b24b86abf3054296d5a54a653e4ec4804d1998f
ritesh.tech <riteshmalav89@gmail.com>
2025-08-26 18:20:32 +0530

    Update hero.js

---
+++ blocks/hero/hero.js
@@ -0,0 +1,11 @@
+.hero
+ {
+    width: 100%;
+    min-height: 80vh;
+    position: relative;
+    padding: 80px 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: var(--default-color);
+}
